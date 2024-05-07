6586c6abe02401b2774eaa6f3065950a2410c3c1
seongeun <seongeun.cho.dev@gmail.com>
2024-05-08 04:56:42 +0900

    .

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -37,6 +37,7 @@ export class TasksController {
   @Post(':id')
   async start(@Param('id') id: string) {
     console.log('1');
+    console.log(id);
     switch (id) {
       // 커뮤니티
       case TASK_MAP.디시인사이드.name:
